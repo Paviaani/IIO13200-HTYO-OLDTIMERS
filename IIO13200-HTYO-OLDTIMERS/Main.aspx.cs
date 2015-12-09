a4238786d5ed22cfbaf922ccd352414eb92a0efb
Ari Salopaa <ari.salopaa@gmail.com>
2015-12-09 23:25:28 +0200

    kysymysten randomointi toimii

--- IIO13200-HTYO-OLDTIMERS/Main.aspx.cs
+++ IIO13200-HTYO-OLDTIMERS/Main.aspx.cs
@@ -29,19 +29,81 @@ namespace IIO13200_HTYO_OLDTIMERS
             docQ.Load(Server.MapPath("~/App_Data/Questions.xml"));
             docA.Load(Server.MapPath("~/App_Data/Answers.xml"));
             int maxQ = docQ.DocumentElement.ChildNodes.Count;
-            int rndNum = randomNumber(0, maxQ-1);
-            rndNum = 0;
+            int rndNum = randomNumber(1, maxQ);
+            int Qindex = findNode(rndNum, docQ);
 
-            string str = docQ.DocumentElement.ChildNodes[rndNum].Attributes.GetNamedItem("ID").Value;
+            string str = docQ.DocumentElement.ChildNodes[Qindex].Attributes.GetNamedItem("ID").Value;
             str += " - ";
-            str += docQ.DocumentElement.ChildNodes[rndNum].InnerText;
+            str += docQ.DocumentElement.ChildNodes[Qindex].InnerText;
 
             lbTest.Text = str;
-            btnAns1.Text = docA.DocumentElement.ChildNodes[rndNum].ChildNodes[0].InnerText;
-            btnAns2.Text = docA.DocumentElement.ChildNodes[rndNum].ChildNodes[1].InnerText;
-            btnAns3.Text = docA.DocumentElement.ChildNodes[rndNum].ChildNodes[2].InnerText;
-            btnAns4.Text = docA.DocumentElement.ChildNodes[rndNum].ChildNodes[3].InnerText;
+
+            fillAnswers(Qindex, docA);
+
         }
+
+        //Function to find XMLNode index with given ID
+        private int findNode(int index, XmlDocument doc)
+        {
+            int count = doc.DocumentElement.ChildNodes.Count;
+            for (int i=0; i<count;i++)
+            {
+                if (doc.DocumentElement.ChildNodes[i].Attributes.GetNamedItem("ID").Value == index.ToString())
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        //Function to randomice answers
+        private void fillAnswers(int index, XmlDocument doc)
+        {
+            int count = doc.DocumentElement.ChildNodes[index].ChildNodes.Count;
+            List<int> luvut = new List<int>();
+            int newNum = 0;
+            int itIsLoop = 0;
+            //Randomice 4 number (node index values)
+            while(luvut.Count < 4 && itIsLoop < 10000)
+            {
+                newNum = randomNumber(0, count-1);
+                itIsLoop++;
+
+                if ( !luvut.Contains(newNum))  //If not all ready exist
+                {
+                    if (luvut.Count == 3)   //if last round check for correct asnwer
+                    {
+                        for (int k = 0; k < count; k++)
+                        {
+                            if (doc.DocumentElement.ChildNodes[index].ChildNodes[k].Attributes.GetNamedItem("att").Value == "True")
+                            {
+                                if (luvut.Contains(k))
+                                {
+                                    luvut.Add(newNum);
+                                }
+                                else
+                                {
+                                    luvut.Add(k);  //add missing answer
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        luvut.Add(newNum);  //add new number
+                    }
+                    
+                }
+            }
+
+            btnAns1.Text = doc.DocumentElement.ChildNodes[index].ChildNodes[luvut[0]].InnerText;
+            btnAns2.Text = doc.DocumentElement.ChildNodes[index].ChildNodes[luvut[1]].InnerText;
+            btnAns3.Text = doc.DocumentElement.ChildNodes[index].ChildNodes[luvut[2]].InnerText;
+            btnAns4.Text = doc.DocumentElement.ChildNodes[index].ChildNodes[luvut[3]].InnerText;
+
+        }
+
+
 
 
         //Function to generate random number
